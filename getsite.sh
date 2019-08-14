77b912d211355465bce820230fae8c3bbda59eb2
paszqa <pasza1989@gmail.com>
2019-08-14 18:20:47 +0200

    path fixed

--- getsite.sh
+++ getsite.sh
@@ -2,15 +2,16 @@
 
 #Set variables
 currentDate=$(date +%Y-%m-%d)
+pathToScript="/home/osmc/git/steamtracker"
 #pasza
 #steamId=76561198000030995
 #pri
 #steamId=76561197994977404
 steamId=$1
-filename=$steamId/site_$currentDate.xml
-filenameTemp=$steamId/tmp_$currentDate.xml
+filename=$pathToScript/users/$steamId/site_$currentDate.xml
+filenameTemp=$pathToScript/users/$steamId/tmp_$currentDate.xml
 #Filesystem ops
-mkdir -p $steamId
+mkdir -p $pathToScript/users/$steamId
 curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=EB73438BE5A148D08473BFDFC8D6EEBC&steamid=$steamId&format=xml&include_appinfo=1&include_played_free_games=1" > $filename
 #REMOVE NEWLINES AND TABS AND SHIT
 cat $filename |grep -ivE "xml version|game_count|response>|<games>|</games>|<message>"|tr -d '\n'|tr -d '\t' > $filenameTemp
@@ -29,9 +30,9 @@ while read line; do
 		appId=$(echo $line|awk -F'appid>' '{print $2}'|tr -d '<'|tr -d '/')
 		playtime=$(echo $line|awk -F'playtime_forever>' '{print $2}'|tr -d '<'|tr -d '/')
 		gameTitle=$(echo $line|awk -F'name>' '{print $2}'|tr -d '<'|tr -d '/')
-		mkdir -p $steamId/$appId
-		echo $gameTitle > $steamId/$appId/info.txt
-		echo "$currentDate;$playtime" > $steamId/$appId/$currentDate
+		mkdir -p $pathToScript/users/$steamId/$appId
+		echo $gameTitle > $pathToScript/users/$steamId/$appId/info.txt
+		echo "$currentDate;$playtime" > $pathToScript/users/$steamId/$appId/$currentDate
 	fi
 done < $filenameTemp
 
