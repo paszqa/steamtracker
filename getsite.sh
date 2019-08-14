79c417aa3370a737988574191f61a02e161ae0e5
root <root@localhost.localdomain>
2019-08-14 14:58:21 +0200

    development

--- getsite.sh
+++ getsite.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#RESET FILES
+
+#Set variables
+currentDate=$(date +%Y-%m-%d)
+steamId=76561198000030995
+#Filesystem ops
+mkdir -p $steamId
 rm -f report
 touch report
-#curl 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=EB73438BE5A148D08473BFDFC8D6EEBC&steamid=76561198000030995&format=xml&include_appinfo=1&include_played_free_games=1' > todaysite.xml
+curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=EB73438BE5A148D08473BFDFC8D6EEBC&steamid=$steamId&format=xml&include_appinfo=1&include_played_free_games=1" > todaysite.xml
 #REMOVE NEWLINES AND TABS AND SHIT
 cat short.xml |grep -ivE "xml version|game_count|response>|<games>|</games>|<message>"|tr -d '\n'|tr -d '\t' > todaysite_nospaces.xml
 #ADD NEWLINES TO XML AT END OF MESSAGE
@@ -10,17 +15,19 @@ sed -i 's#</message>#</message>\n#g' todaysite_nospaces.xml
 #REMOVE AMPS
 sed -i 's#\&amp;#\&#g' todaysite_nospaces.xml
 #FIX APOSTROPHES
-sed -i "s#\&apos;#\'#g" todaysite_nospaces.xml
+sed -i "s#\&apos;#'#g" todaysite_nospaces.xml
 
 #cat todaysite_nospaces.xml | awk -F'</message>' '{print $2}'|awk -F'appid>' '{print $2}'|tr -d '<'|tr -d '/'; echo ""
 while read line; do
 #	echo "X"
 #	echo "RUN: $line" >> ids_only
 	if [[ $line != "" ]] && [[ $line != " " ]]; then
-		appid=$(echo $line|awk -F'appid>' '{print $2}'|tr -d '<'|tr -d '/')
+		appId=$(echo $line|awk -F'appid>' '{print $2}'|tr -d '<'|tr -d '/')
 		playtime=$(echo $line|awk -F'playtime_forever>' '{print $2}'|tr -d '<'|tr -d '/')
-		gametitle=$(echo $line|awk -F'name>' '{print $2}'|tr -d '<'|tr -d '/')
-		echo "$appid;$gametitle;$playtime;" >> report
+		gameTitle=$(echo $line|awk -F'name>' '{print $2}'|tr -d '<'|tr -d '/')
+		mkdir -p $steamId/$appId
+		echo $gameTitle > $steamId/$appId/info.txt
+		echo "$currentDate;$playtime" > $steamId/$appId/$currentDate
 	fi
 done < todaysite_nospaces.xml
 
