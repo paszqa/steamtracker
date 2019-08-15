958b5f5efbeaa6b2452e471caa1e9ca7581915f7
paszqa <pasza1989@gmail.com>
2019-08-15 02:26:18 +0200

    commit

--- countdiff.sh
+++ countdiff.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
-fromDate=$1
-#previousDate=$(date $fromDate -d "yesterday")
+if [ -z $2 ]; then
+	fromDate=$(date +%Y-%m-%d)
+else
+	fromDate=$2
+fi
 previousDate=$(date -d "$fromDate -1 day" +%Y-%m-%d)
+pathToScript="/home/osmc/git/steamtracker"
+steamId=$1
+#if file doesnt exist,create it
+touch $pathToScript/users/$steamId/played.csv
 
-echo date=$fromDate
-echo prev=$previousDate
-steamId=76561198000030995
-for folder in $(ls $steamId); do
+for folder in $(ls $pathToScript/users/$steamId/ | grep -viE "xml|csv"); do
+	appId=$folder
+	folder=$pathToScript/users/$steamId/$folder
 	#echo $steamId
 	#echo $folder
 	#cat $steamId/$folder/$fromDate
 	#cat $steamId/$folder/$fromDate | awk -F";" "{ print $2 }"
-	todayGameTime=$(cat $steamId/$folder/$fromDate | awk -F";" '{ print $2 }')
-	yesterdayGameTime=$(cat $steamId/$folder/$previousDate| awk -F";" '{ print $2 }')
+	todayGameTime=$(cat $folder/$fromDate | awk -F";" '{ print $2 }')
+	if [ -f "$folder/$previousDate" ]; then
+		yesterdayGameTime=$(cat $folder/$previousDate| awk -F";" '{ print $2 }')
+	else
+		yesterdayGameTime=0
+	fi
+	gameTitle=$(cat $folder/info.txt)
 	#echo "t:$todayGameTime"
 	#echo "y:$yesterdayGameTime"
 	#echo -n $fromDate >> $steamId/$folder/played.txt
-	echo "$todayGameTime - $yesterdayGameTime" | bc
+	#echo "$todayGameTime - $yesterdayGameTime" | bc
 	playedTodayOnly=$(echo "$todayGameTime - $yesterdayGameTime" | bc) 
-	echo "$fromDate;$playedTodayOnly" >> $steamId/$folder/played.txt
+	if [ $playedTodayOnly -gt 0 ]; then
+		echo "$fromDate;$appId;$gameTitle;$playedTodayOnly;$todayGameTime" >> $pathToScript/users/$steamId/played.csv
+	fi
 	#echo "X:$playedTodayOnly"
 	
 done
