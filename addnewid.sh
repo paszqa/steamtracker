0f458ff09ed569239f76759649a5518baa979fe1
paszqa <pasza1989@gmail.com>
2019-08-22 20:01:08 +0200

    Moved files to dirs, fixed adding new users

--- addnewid.sh
+++ addnewid.sh
@@ -4,10 +4,10 @@ currentDate=$(date +%Y-%m-%d)
 pathToScript="/home/osmc/git/steamtracker"
 mkdir -p $pathToScript/users/$steamId
 if [ -f $pathToScript/users/$steamId/datejoined.txt]; then
-	echo "Users exists"
+	echo "User exists"
 else
 	echo $currentDate > $pathToScript/users/$steamId/datejoined.txt
 fi
-$pathToScript/getsite.sh $steamId
-$pathToScript/calcMonth.sh $steamId
-$pathToScript/generateWeb.sh $steamId
+#$pathToScript/getsite.sh $steamId
+#$pathToScript/calcMonth.sh $steamId
+#$pathToScript/generateWeb.sh $steamId
