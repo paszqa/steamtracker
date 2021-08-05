c2d1c3a5ae04af7b0a81a46935167f2d999f780b
paszqa <pasza1989@gmail.com>
2021-08-05 23:38:39 +0100

    Updated.

--- makeallphp.sh
+++ makeallphp.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo > /var/www/html/steamtracker/all.php
-ls -1 /var/www/html/steamtracker/ | while read y; do
+pathToWebDir="/var/www/localhost/steamtracker"
+echo > $pathToWebDir/all.html
+ls -1 $pathToWebDir/ | while read y; do
 #        echo "Y: $y"
 #       if [[ $y != "all.php" ]] && [[ $y == *".php"* ]] && [[ $y != "-raw.php" ]]; then
-	if [[ $y != "all.php" ]] && [[ $y == *".php"* ]] && [[ ${y} != *"-raw.php"* ]] && [[ $y != *"top5-ever.php"* ]]; then
+	if [[ $y != "all.html" ]] && [[ $y == *".html"* ]] && [[ ${y} != *"-raw.php"* ]] && [[ $y != *"top5-ever.php"* ]]; then
 #                echo "........Y2: $y"
-                echo "<p><a href='$y'>$y</a></p>" >> /var/www/html/steamtracker/all.php
+                echo "<p><a href='$y'>$y</a></p>" >> $pathToWebDir/all.html
         fi
 done
